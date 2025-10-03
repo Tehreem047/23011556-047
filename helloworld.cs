d7160db048a3d8cb0985a01b09e0295049e974a9
Tehreem047 <tehreem112@outlook.com>
2025-10-03 20:15:38 +0500

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfirstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the first program in Github");
+        }
+    }
+}
